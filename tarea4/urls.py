e9ee8cc352aed64c768c15cd28c1b211dfdeb33b
matirojas <matias.rojas.g@ug.uchile.cl>
2018-11-06 14:33:30 -0300

    cambios pequeños

--- tarea4/urls.py
+++ tarea4/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     # path('', views.index, name=''),
     # En este caso el string de path recibe un entero, y la variable tiene nombre humano. También se pueden usar regex.
     # El nombre de la url permite insertarla en la template usando {% url 'humano' %}
-    path('home', views.landingPageEstudiante)
+    path('admin/', admin.site.urls),
+    path('home/', views.landingPageEstudiante),
+    path('perfil/', views.perfilDueno),
+    path('fichaCursoDoc/', views.fichaCursoDocente),
+    path('fichaCursoEst/', views.fichaCursoEstudiante),
+    path('fichaCoev/', views.fichaCoevaluacionEstudiante),
 ]
--- tarea4/views.py
+++ tarea4/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from tarea4.models import *
 
+
 # Create your views here.
 def landingPageEstudiante(request):
-    userID = 1; #placeholder para la id correcta.
+    userID = 1;  # placeholder para la id correcta.
 
-    #nota dependiendo de como quede el modelo user, el filtro puede cambiar.
+    # nota dependiendo de como quede el modelo user, el filtro puede cambiar.
     cursosEstudiante = Curso.objects.filter(estudiante__user_id=userID)
     cursosDocente = Curso.objects.filter(docente__user_id=userID)
-
 
     coevaluacionesDocente = Coevaluacion.objects.filter(pregunta__preguntasdocentes__docente_id=userID)
     coevaluacionesEstudiante = Coevaluacion.objects.filter(pregunta__preguntasestudiantes__estudiante=userID).distinct()
@@ -18,16 +18,18 @@ def landingPageEstudiante(request):
 
 def perfilDueno(request):
 
-    return render()
+    return render(request, 'tarea4/perfilDueno.html')
+
 
 def fichaCursoEstudiante(request):
+    return render(request, 'tarea4/fichaCursoEstudiante.html')
 
-    return  render()
 
 def fichaCursoDocente(request):
+    return render(request, 'tarea4/fichaCursoDocente.html')
 
-    return render()
 
 def fichaCoevaluacionEstudiante(request):
-
-    return render()
+    cursoID = 1;
+    coev = Coevaluacion.objects.filter(curso=cursoID)
+    return render(request, 'tarea4/fichaCoevaluacionEstudiante.html', {'coev': coev})
