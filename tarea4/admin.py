c73031a7a20bd01c98f700876ba6f4e45cd206a9
Alex Alvarez Toledo <otajla@hotmail.com>
2018-11-06 03:00:58 -0300

    agregado a admin la base de datos

--- tarea4/admin.py
+++ tarea4/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from tarea4.models import *
 
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Docente)
+admin.site.register(Estudiante)
+admin.site.register(Grupo)
+admin.site.register(Coevaluacion)
+admin.site.register(Pregunta)
+admin.site.register(PreguntasEstudiantes)
+admin.site.register(PreguntasDocentes)
