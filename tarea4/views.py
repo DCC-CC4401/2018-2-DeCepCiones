9a4e2a47def5056e4d705069e44c15ee3b65c39a
drusill <raul.cid@ing.uchile.cl>
2018-11-20 18:17:32 -0300

    Cambiar contraseña en perfil dueño Closes #6

--- tarea4/views.py
+++ tarea4/views.py
@@ -1,44 +1,48 @@
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render
 from tarea4.models import *
+from django.contrib.auth import *
+from django.contrib import messages
 
 
 # Create your views here.
 def landingPageEstudiante(request):
     userID = request.user.id  # placeholder para la id correcta.
-
-    # nota dependiendo de como quede el modelo user, el filtro puede cambiar.
-    cursosEstudiante = Curso.objects.filter(estudiante__user_id=userID)
-    cursosDocente = Curso.objects.filter(docente__user_id=userID)
-    docentes = Docente.objects.filter(user_id=userID)
-    coevaluaciones = Coevaluacion.objects.filter(curso__estudiante__user_id=userID)
-    # coevaluaciones[0].curso.docente_set.filter(user_id__exact=userID)[0].cargo
-
+    cursos = Curso.objects.filter(usuariocurso__user=userID)
+    coevals = Coevaluacion.objects.filter(curso__usuariocurso__user=userID).order_by('fecha_termino')
     listaCurso = []
     listaCoev = []
-    for curso in cursosEstudiante:
-        listaCurso.append({"cargo":"alumno", "nombre":curso.Nombre, "codigo":curso.Codigo+str(curso.Seccion), "semestre":str(curso.Ano)+" - "+str(curso.Semestre), "id":curso.id})
+    for curso in cursos:
+        listaCurso.append({"cargo": curso.usuariocurso_set.get(user=userID).cargo, "nombre": curso.Nombre,
+                           "codigo": curso.Codigo + str(curso.Seccion),
+                           "semestre": str(curso.Ano) + " - " + str(curso.Semestre),
+                           "id": curso.id})
 
-    for curso in cursosDocente:
-        listaCurso.append({"cargo": curso.docente_set.get(user_id__exact=userID).cargo, "nombre":curso.Nombre, "codigo":curso.Codigo+str(curso.Seccion), "semestre":str(curso.Ano)+" - "+str(curso.Semestre), "id":curso.id})
+    for coev in coevals:
+        # No estamos haciendo los estados como secundarios
+        listaCoev.append({'estadoTr': coev.estado, 'fechaInicio': coev.fecha_inicio, 'nombre': coev.nombre,
+                          'cursoNombre': coev.curso.Nombre, 'cursoCod': coev.curso.Codigo,
+                          'cursoSemestre': str(coev.curso.Ano) + "-" + str(coev.curso.Semestre),
+                          'fechaFin': coev.fecha_termino, 'estado': coev.estado, 'responder': 'responder',
+                          'cargo': coev.curso.usuariocurso_set.get(user=userID).cargo, "id": coev.id})
 
-    for coev in coevaluaciones:
-        # Revisar si hay cargo docente en el curso
-        listaCoev.append({'estadoTr': coev.estado, 'fechaInicio': coev.fecha_inicio, 'nombre': coev.nombre, 'cursoNombre': coev.curso.Nombre, 'cursoCod': coev.curso.Codigo, 'cursoSemestre': str(coev.curso.Ano) + "-" + str(coev.curso.Semestre),
-                          'fechaFin': coev.fecha_termino, 'estado': coev.estado, 'responder': 'responder', 'cargo': "alumno","id": coev.id})
-
-    #micoev = [{'estadoTr': "pendiente", 'fechaInicio': "asda", 'nombre': "ajaja", 'cursoNombre': "asdads", 'cursoCod':"poiopoi", 'cursoSemestre':"asd", 'fechaFin': "asda", 'responder':"Responder", 'estado':"Pendiente", 'cargo': "alumno"}]
-    #micurso = [{'cargo': "alumno", 'nombre':"asda", 'codigo':"67890", 'semestre':"67890p"}]
-
-
-    return render(request, 'landingPageEstudiante.html', {'listaCoev': listaCoev, 'listaCurso': listaCurso  })
+    return render(request, 'landingPageEstudiante.html', {'listaCoev': listaCoev, 'listaCurso': listaCurso})
 
 
 def perfilDueno(request):
-    userID = 1 #placeholder
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'contraseña cambiada')
+
+    userID = request.user.id    #placeholder
     listaCurso = Curso.objects.filter(estudiante__user_id__exact=userID)
     listaCoev = Coevaluacion.objects.filter(curso__estudiante__user_id=userID)
-    estudiante = Estudiante.objects.get(user_id__exact=userID)
+    estudiante = User.objects.get(user_id__exact=userID)
     cursosEstudiante = []
+
 
     for curso in listaCurso:
         cursosEstudiante.append({'cargo':"alumno", 'nombre':curso.Nombre, 'codigo': curso.Codigo + "-" + str(curso.Seccion),
@@ -46,7 +50,9 @@ def perfilDueno(request):
     dueno = {'nombre': estudiante.nombre, 'nombreCompleto': estudiante.nombre + " " + estudiante.apellido,
              'email': estudiante.email, 'rut': estudiante.rut}
 
-    return render(request, 'perfilDueno.html', {'dueno': dueno, 'listaCurso': cursosEstudiante})
+    form = PasswordChangeForm(user=request.user)
+
+    return render(request, 'perfilDueno.html', {'dueno': dueno, 'listaCurso': cursosEstudiante, 'contraseñaForm': form, 'listaCoev': listaCoev})
 
 
 def fichaCursoEstudiante(request, idCurso):
