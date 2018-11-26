35066639ae14b664c14a105c63b3a7be8d8767f7
Alex Alvarez Toledo <otajla@hotmail.com>
2018-11-26 00:48:54 -0300

    estilo del login

---
+++ tarea4/static/tarea4/js/loginStyle.js
@@ -0,0 +1,16 @@
+
+var l;
+var i;
+
+var labelList = document.getElementById("logForm").getElementsByTagName("LABEL");
+
+document.getElementById("id_username").classList.add("form-control");
+document.getElementById("id_password").classList.add("form-control");
+
+document.getElementById("logForm").getElementsByTagName("BUTTON")[0].classList.add("btn");
+document.getElementById("logForm").getElementsByTagName("BUTTON")[0].classList.add("btn-lg");
+document.getElementById("logForm").getElementsByTagName("BUTTON")[0].classList.add("btn-primary-custom");
+document.getElementById("logForm").getElementsByTagName("BUTTON")[0].classList.add("px-5");
+document.getElementById("logForm").getElementsByTagName("BUTTON")[0].classList.add("mt-3");
+
+console.log("AQUI!");
